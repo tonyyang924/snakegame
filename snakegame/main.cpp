879e44bcd8fa9659a1b18f4efc272c4bb592e48c
Tony Yang <superzoro168@gmail.com>
2020-05-25 22:31:29 +0800

    Initial Commit

---
+++ snakegame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  snakegame
+//
+//  Created by Tony Yang on 2020/5/25.
+//  Copyright © 2020 Tony Yang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
